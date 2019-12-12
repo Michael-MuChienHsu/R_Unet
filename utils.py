fbf7379bdd3895f337257a16a530533f33ca605f
tomorrow_morning <44192532+vagr8@users.noreply.github.com>
2019-12-12 15:47:23 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,38 +10,20 @@ def load_image(path):
     img = cv.imread(path)
     return img
 
-def reshape_cv( img, gray_scale_r = False ):
-    if gray_scale_r == True:
-        gray_scale_factor = 1
+def reshape_cv(img, gray_scale_r=False):
+    if not gray_scale_r:
+        return np.reshape(img, (3, 256, 256))
     else:
-        gray_scale_factor = 3
+        return np.reshape(img, (1, 256, 256))
 
-    new_img = np.zeros((gray_scale_factor, 256, 256), dtype=float)
-    for i in range(0, 256):
-        for j in range(0, 256):
-            if gray_scale_r == False:
-                for k in range(0, 3):
-                    new_img[k][i][j] = img[i][j][k]
-            if gray_scale_r == True:
-                new_img[0][i][j] = img[i][j]
-    
-    return new_img
-
+# this is stupid 
 def pic_normalize(pic):
-    w = pic.shape[0]
-    h = pic.shape[1]
-    c = pic.shape[2]
+    pic = np.asarray( pic, dtype=float )
+    pic = pic/256
 
-    new_pic = np.zeros((w,h,c), dtype=float)
-    for i in range(0, w):
-        for j in range(0, h):
-            for k in range(0, c):    
-                pic[i][j][k] = float(pic[i][j][k])
-                new_pic[i][j][k] = pic[i][j][k]/256.0
-    
-    return new_pic
+    return pic
 
-def load_pic(f_num, f_path, nomalize = False, gray_scale = False):
+def load_pic(f_num, f_path, normalize = False, gray_scale = False):
     path_in = f_path[f_num]
     path_target = f_path[f_num+1]
 
@@ -53,8 +35,8 @@ def load_pic(f_num, f_path, nomalize = False, gray_scale = False):
         tar_pic = cv.imread(path_target)
 
 
-    ## nomalize pic for 0-256 to 0-1
-    if nomalize == True:
+    ## normalize pic for 0-256 to 0-1
+    if normalize == True:
         in_pic = pic_normalize(in_pic)
         tar_pic = pic_normalize(tar_pic)
 
@@ -74,33 +56,20 @@ def load_pic(f_num, f_path, nomalize = False, gray_scale = False):
      
     return input_pic, target_pic
 
+
 def tensor_to_pic(tensor, normalize = False, gray_scale = False):
-    if gray_scale == True:
-        gray_scale_factor = 1
+    tensor = tensor.detach().numpy()
     if gray_scale == False:
-        gray_scale_factor = 3
+        img = np.reshape(tensor, (256, 256, 3))
+    else:
+        img = np.reshape(tensor, (256, 256))
 
-    tensor = tensor.view(gray_scale_factor, 256, 256)
-    ## reshape
-    new_img = np.zeros((256, 256, gray_scale_factor), dtype=float)
-    for i in range(0, 256):
-        for j in range(0, 256):
-            for k in range(0, gray_scale_factor):
-                new_img[i][j][k] = tensor[k][i][j]
-    # anti_normalize
-    w = new_img.shape[0]
-    h = new_img.shape[1]
-    c = new_img.shape[2]
-    out_img = np.zeros((256, 256, gray_scale_factor), dtype=np.uint8)
-    for i in range(0, w):
-        for j in range(0, h):
-            for k in range(0, c):
-                if normalize == True:
-                    out_img[i][j][k] = int(new_img[i][j][k]*256)
-                else:
-                    out_img[i][j][k] = int(new_img[i][j][k])
+    if normalize==True:     
+        img = img*256
 
-    return out_img
+    img = np.asarray(img, dtype=np.uint8)
+
+    return img
 
 def write_csv_file( filename, data):    
     with open(filename, 'w', newline='') as csvfile:
@@ -126,26 +95,9 @@ if __name__ == "__main__":
             if ".jpg" in file:
                 filepath = "./origami_single/" + file
                 frame_paths.append(filepath)
-    test, target = load_pic( 1, frame_paths )
+    test, target = load_pic( 1, frame_paths, normalize=True, gray_scale=True )
 
-    img = tensor_to_pic(test)
+    img = tensor_to_pic(test,normalize=True, gray_scale=True)
     cv.imwrite('color_img.jpg', img)
     cv.imshow('My Image', img)
     cv.waitKey(0)
-
-    '''
-    img = cv.imread('./origami_single/000.jpg')
-    ##cv.imshow('My Image', img)
-    ##cv.waitKey(0)
-    img = cv.resize(img, (256,256), interpolation=cv.INTER_CUBIC )
-    ##cv.imshow('My Image', img)
-    ##cv.waitKey(0)
-    img = pic_normalize(img)
-    img = reshape_cv(img)
-    tensor = torch.tensor( img, dtype=torch.float )
-
-    img = tensor_to_pic(tensor, normalize=True)
-    cv.imwrite('color_img.jpg', img)
-    cv.imshow('My Image', img)
-    cv.waitKey(0)
-    '''
