52b7e03874b29e7c2f1b526b35411f01b1a0a850
tomorrow_morning <44192532+vagr8@users.noreply.github.com>
2019-12-19 23:31:29 +0900

    Update parse_argument.py

--- parse_argument.py
+++ parse_argument.py
@@ -6,10 +6,12 @@ class argrements():
         self.videopath = ''
         self.step = ''
         self.epoch_num = ''
+        self.output_path = ''
         self.gray_scale = ''
         self.sz_idx = ''
         self.lr = ''
         self.loss_func = ''
+        self.predict_frame = ''
         self.parseJSON()
     
     def parseJSON(self):
@@ -21,12 +23,14 @@ class argrements():
             try:
                 config = json.load(json_file)
                 self.videopath = config["videopath"]
+                self.output_path = config["output_path"]
                 self.step = config["step"]        
                 self.sz_idx = config["size_idx"]
                 self.loss_func = config["loss_function"]
                 self.epoch_num = config["epoch"]
                 self.lr = config["learning_rate"]
                 self.gray_scale = config["gray_scale"]
+                self.predict_frame = config["predict_frame"]
                 if self.gray_scale == 'True':
                     self.gray_scale = True
                 else:
