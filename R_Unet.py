e1c36423981a3359853cb5cfc6e30654b7f2cff9
tomorrow_morning <44192532+vagr8@users.noreply.github.com>
2019-12-19 09:18:03 +0900

    Update R_Unet.py

--- R_Unet.py
+++ R_Unet.py
@@ -8,7 +8,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
 import os
-import time
+import gc
 
 # Down convolution layer
 class Down_Layer(nn.Sequential):
@@ -64,61 +64,85 @@ class Up_Layer(nn.Sequential):
         return output
 
 class recurrent_network(nn.Sequential):
-    def __init__(self, hidden_layer1, hidden_layer2, use_buffer = False):
+    def __init__(self, fraction_index = 1):
+        cuda_gpu = torch.cuda.is_available()
+        self.resize_fraction = fraction_index
         super(recurrent_network, self).__init__()
-        self.use_buffer = use_buffer
-        self.rnn = nn.LSTM(16, 16)
-        self.hidden1 = hidden_layer1
-        self.hidden2 = hidden_layer2
-        if use_buffer == True:
-            self.output_buffer = []
+        self.rnn = nn.LSTM(int(16/fraction_index), int(16/fraction_index) )
+        if cuda_gpu:
+            self.hidden1 = torch.zeros(1, int(16/self.resize_fraction), int(16/self.resize_fraction)).cuda()
+            self.hidden2 = torch.zeros(1, int(16/self.resize_fraction), int(16/self.resize_fraction)).cuda()
+        else:
+            self.hidden1 = torch.zeros(1, int(16/self.resize_fraction), int(16/self.resize_fraction))
+            self.hidden2 = torch.zeros(1, int(16/self.resize_fraction), int(16/self.resize_fraction))
 
     def forward(self, x):
-        if self.use_buffer == False:
-            for i in x:
-            # Step through the sequence one element at a time.
-            # after each step, hidden contains the hidden state.
-                out, (self.hidden1, self.hidden2) = self.rnn(i, (self.hidden1, self.hidden2) )
-            
-            return out
+        for i in x:
+        # Step through the sequence one element at a time.
+        # after each step, hidden contains the hidden state.
+            out, (self.hidden1, self.hidden2) = self.rnn(i, (self.hidden1, self.hidden2) )
 
+        return out
+
+class recurrent_network_layer(nn.Sequential):
+    def __init__(self, fraction_index = 1):
+        super(recurrent_network_layer, self).__init__()
+        cuda_gpu = torch.cuda.is_available()
+        self.rnn = nn.LSTM(int(16/fraction_index), int(16/fraction_index) )
+        self.resize_fraction = fraction_index
+        self.free_mem_counter = 0
+        if cuda_gpu:
+            self.hidden1 = torch.zeros(1, int(16/self.resize_fraction), int(16/self.resize_fraction)).cuda()
+            self.hidden2 = torch.zeros(1, int(16/self.resize_fraction), int(16/self.resize_fraction)).cuda()
         else:
+            self.hidden1 = torch.zeros(1, int(16/self.resize_fraction), int(16/self.resize_fraction))
+            self.hidden2 = torch.zeros(1, int(16/self.resize_fraction), int(16/self.resize_fraction))
+        self.output_buffer = []
+
+    def forward(self, x):
+        self.init_buffer()
+        for i in x:
+        # Step through the sequence one element at a time.
+        # after each step, hidden contains the hidden state.
+            out, (self.hidden1, self.hidden2) = self.rnn(i, (self.hidden1, self.hidden2) )
+            self.output_buffer.append(out)
+            out.clone()
+            del out
+        
+        return self.output_buffer
+    
+    def init_buffer(self):
+        if len(self.output_buffer) > 0:
             self.output_buffer = []
-            for i in x:
-            # Step through the sequence one element at a time.
-            # after each step, hidden contains the hidden state.
-                out, (self.hidden1, self.hidden2) = self.rnn(i, (self.hidden1, self.hidden2))
-                self.output_buffer.append(out)
-            return self.output_buffer
     
 class unet(nn.Module):
-    def __init__(self, tot_frame_num = 100, length = 6, Gary_Scale = False):
+    def __init__(self, tot_frame_num = 100, length = 6, Gary_Scale = False, size_index = 256):
+        print("gray scale:", Gary_Scale)
         super( unet, self ).__init__()
+        if size_index != 256:
+            self.resize_fraction = window_size = 256/size_index
+        else:
+            self.resize_fraction = 1
+
         cuda_gpu = torch.cuda.is_available()
 
-        if cuda_gpu:
-            self.hidden11 = torch.zeros(1, 16, 16).cuda()  # (hidden_layer num, second_dim, output channel)
-            self.hidden12 = torch.zeros(1, 16, 16).cuda()
-            self.hidden21 = torch.zeros(1, 16, 16).cuda()  # (hidden_layer num, second_dim, output channel)
-            self.hidden22 = torch.zeros(1, 16, 16).cuda()
-        else:
-            self.hidden11 = torch.zeros(1, 16, 16)  # (hidden_layer num, second_dim, output channel)
-            self.hidden12 =  torch.zeros(1, 16, 16)
-            self.hidden21 = torch.zeros(1, 16, 16)  # (hidden_layer num, second_dim, output channel)
-            self.hidden22 = torch.zeros(1, 16, 16)
+
+        self.latent_feature = 0
 
         self.step = length
+        self.free_mem_counter = 0
         self.max_pool = nn.MaxPool2d(2)
         self.upsample = nn.UpsamplingBilinear2d(scale_factor=2)
         self.one_conv1 = nn.Conv2d( 1024, 512, kernel_size=1, bias=True)
         self.one_conv2 = nn.Conv2d( 1024, 512, kernel_size=1, bias=True)
 
-        self.rnn = recurrent_network( self.hidden11, self.hidden12, use_buffer = True )
-        self.rnn2 = recurrent_network( self.hidden21, self.hidden22 )
+        self.rnn = recurrent_network_layer( fraction_index = self.resize_fraction )
+        self.rnn2 = recurrent_network( fraction_index = self.resize_fraction )
 
-        self.down1 = Down_Layer( 3, 64 )
         if Gary_Scale == True:
             self.down1 = Down_Layer(1, 64)
+        else:
+            self.down1 = Down_Layer( 3, 64 )
 
         self.down2 = Down_Layer( 64, 128 )
         self.down3 = Down_Layer( 128, 256 )
@@ -129,21 +153,15 @@ class unet(nn.Module):
         self.up2 = Up_Layer(512, 256)
         self.up3 = Up_Layer(256, 128)
         self.up4 = Up_Layer(128, 64)
-        self.up5 = nn.Conv2d( 64, 3, kernel_size = 1 )
         if Gary_Scale == True:
             self.up5 = nn.Conv2d( 64, 1, kernel_size = 1 )
-    '''
-    ## move to utils.py
-    def buffer_update(self, latent_feature):
-        if len(self.lstm_buf) == self.step:
-            for i in range(0, self.step-1):
-                self.lstm_buf[i] = self.lstm_buf[i+1]
-            self.lstm_buf[self.step-1] = latent_feature
         else:
-            self.lstm_buf.append( latent_feature )
-    '''
+            self.up5 = nn.Conv2d( 64, 3, kernel_size = 1 )
+    
     def forward(self, x, buffer):
         self.lstm_buf = buffer.copy()
+        self.free_mem_counter = self.free_mem_counter + 1
+        #self.lstm_buf = []
 
         # down convolution
         x1 = self.down1(x)
@@ -160,21 +178,21 @@ class unet(nn.Module):
         
         x5 = self.down5(x5)
 
-        latent_feature = x5.view(-1, 16, 16)
-        self.lstm_buf.append( latent_feature )
-        print( 'lstm buffer len', len( self.lstm_buf ) )
+        latent_feature = x5.view(-1, int(16/self.resize_fraction), int(16/self.resize_fraction) )
+
+        self.lstm_buf.append(latent_feature )
+        # print( 'lstm buffer len', len( self.lstm_buf ) )
         # LSTM unit
         if len( self.lstm_buf ) > 1 :
             lstm_output = self.rnn(self.lstm_buf)
             lstm_output = self.rnn2( lstm_output )
-            lstm_output = lstm_output.view(1, 1024, 16, 16)
-        
+            lstm_output = lstm_output.view(1, 1024, int(16/self.resize_fraction), int(16/self.resize_fraction) )
+
         # use x5 to perform lstm
         if 'lstm_output' in locals():
             x6 = self.one_conv1(lstm_output)
             x5 = self.one_conv2(lstm_output)
             x5 = torch.cat((x5, x6), dim = 1)   
-            del lstm_output
         
         # up convolution
         x = self.up1( x5, x4 )
@@ -185,5 +203,21 @@ class unet(nn.Module):
 
         ## release var
         self.lstm_buf = []
+        
+        del x1, x2, x3, x4, x5
+        if 'x6' in locals():
+            del x6
+        gc.collect()
+
+        if self.free_mem_counter == 10 :
+            self.free_memory()
 
         return x, latent_feature
+
+    def free_memory(self):
+        self.rnn.hidden1 = self.rnn.hidden1.detach()
+        self.rnn.hidden2 = self.rnn.hidden2.detach()
+        self.rnn2.hidden1 = self.rnn2.hidden1.detach()
+        self.rnn2.hidden2 = self.rnn2.hidden2.detach()
+        self.free_mem_counter = 0
+
